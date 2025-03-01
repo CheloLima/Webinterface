b360d7817bac1061b25283147aa27e17c1f265dc
Chelo Lima <chelolima.pe@gmail.com>
2025-03-01 02:28:52 +0100

    Update constants.ts
    Signed-off-by: Chelo Lima <chelolima.pe@gmail.com>

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_PATH = import.meta.env.VITE_API_URL || "http://37.221.93.77:8888" // <- Edit this string to your own API
+export const BASE_PATH = import.meta.env.VITE_API_URL || "http://api.69.chelo.click/" // <- Edit this string to your own API
 export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://invite.69.chelo.click" // <- Edit this string to your Invite link
 
 export function get(path: string) {
